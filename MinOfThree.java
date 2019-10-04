70d5edc451367b10d90045582090a544de1eb7b3
Michael Talbert <michael-talbert>
2019-10-04 09:13:49 -0700

    formatted

--- MinOfThree.java
+++ MinOfThree.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class MinOfThree{
 
     public static void main (String [] args){
-      System.out.println("Please input three numbers");
+        System.out.println("Please input three numbers");
+
+
         Scanner scan = new Scanner(System.in);
+
+
         int num1 = scan.nextInt();
         int num2 = scan.nextInt();
         int num3 = scan.nextInt();
+
+
         if ((num2>=num1)&&(num3>=num1)){
           System.out.println("The smallest number is:"+num1);
         }
