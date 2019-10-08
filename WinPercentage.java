b6bd316083bf5047545b73d2c748ab52b35d9b28
Michael Talbert <michael-talbert>
2019-10-08 09:53:54 -0700

    Win percentage update

--- WinPercentage.java
+++ WinPercentage.java
@@ -6,23 +6,27 @@ public class WinPercentage{
   public static void main(String[] args){
     System.out.println("Please input the number of games you have played");
     Scanner scan = new Scanner(System.in);
-    double gamesPlayed = scan.nextInt();
+    double gamesPlayed = scan.nextDouble();
     System.out.println("Please input the number of games won");
-    double gamesWon = scan.nextInt();
+    double gamesWon = scan.nextDouble();
 
-    while( (gamesPlayed<=0) || (gamesWon>gamesPlayed) || (gamesWon<0) ){
+    while( (gamesPlayed<=0) || (gamesWon>gamesPlayed) || (gamesWon<0) || (Math.round(gamesWon)!=gamesWon) || (Math.round(gamesPlayed)!=gamesPlayed)){
       if(gamesPlayed<=0){
-      System.out.println("total games played must be greater than zero");
+      System.out.println("total games played must be greater than zero\n");
     } else if(gamesWon>gamesPlayed){
-      System.out.println("you haven't played enough games to win that many");
+      System.out.println("you haven't played enough games to win that many\n");
     } else if(gamesWon<0){
-      System.out.println("you can't win negative games");
+      System.out.println("you can't win negative games\n");
+    } else if(Math.round(gamesWon)!=gamesWon){
+      System.out.println("games have to be a whole number \n");
+    }else if(Math.round(gamesPlayed)!=gamesPlayed){
+      System.out.println("games have to be a whole number \n");
     }
     System.out.println("Please input your numbers again");
     System.out.println("Games Played?");
-    gamesPlayed = scan.nextInt();
+    gamesPlayed = scan.nextDouble();
     System.out.println("Games Won?");
-    gamesWon = scan.nextInt();
+    gamesWon = scan.nextDouble();
     }
     double winPercentage = gamesWon/gamesPlayed;
     winPercentage = winPercentage*100;
