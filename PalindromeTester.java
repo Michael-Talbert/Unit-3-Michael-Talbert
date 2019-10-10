d3f485e0ab8829097e361eeb823a422fa3e279bd
Michael Talbert <michael-talbert>
2019-10-10 09:54:13 -0700

    Im not a fan of the palindrome tester thing

---
+++ PalindromeTester.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+
+
+public class PalindromeTester{
+
+  public static void main(String[] args){
+      Scanner scan = new Scanner(System.in);
+
+      System.out.println("Please input a word or phrase to test if it is a palnidrome");
+      String originalStatment = scan.nextLine();
+      String lowercaseString = testStatment.toLowercase();
+      boolean palindrome = true;
+      int counter = 0;
+      while (lowercastTest.length()!=counter){
+
+
+      }
+
+  }
+}
